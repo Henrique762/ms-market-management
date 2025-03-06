d5713e5ef06b5256b80ee39cc7f373e5082eb4c8
Henrique Gil <henriquegil123@gmail.com>
2025-03-06 15:13:52 -0300

    feature/validation

---
+++ src/route.py
@@ -0,0 +1,12 @@
+from config.config import app, request, jsonify
+from Application.Controllers.cadastro import validacao
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    forms_cadastro = request.get_json()
+    validacao_retorno = validacao(forms_cadastro)
+    return jsonify(validacao_retorno)
+
+
+if __name__ == '__main__':
+    app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])

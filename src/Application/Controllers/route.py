a16057da660ee9eec14fa36a032bde2002e33416
Henrique <henriquegil123@gmail.com>
2025-03-21 12:02:06 -0300

    feature/alterando_rota

--- src/Application/Controllers/route.py
+++ src/Application/Controllers/route.py
@@ -4,7 +4,7 @@ from src.Application.Validators.cadastro import create_user
 
 cadastro_blueprint = Blueprint('cadastro', __name__, url_prefix='/api')
 
-@cadastro_blueprint.route('/cadastro', methods=['POST'])
+@cadastro_blueprint.route('/sellers', methods=['POST'])
 def cadastro():
     forms_cadastro = request.get_json()
     usuario = create_user(forms_cadastro)
